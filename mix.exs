b4740e6a02c014bdd3645a7628c21a3f4d2f2345
Pedro Assunção <assuncas@gmail.com>
2022-10-10 17:08:50 +0100

    Removing unneeded phoenix from extra_applications

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ObanUi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :phoenix]
+      extra_applications: [:logger]
     ]
   end
 
