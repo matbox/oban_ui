d6bdfe97340247b2fd3df8abef8454ee01fc7d40
Pedro Assunção <assuncas@gmail.com>
2022-10-11 16:47:30 +0100

    Whoops

--- lib/oban_ui/live/jobs/index.ex
+++ lib/oban_ui/live/jobs/index.ex
@@ -1,11 +1,3 @@
-defmodule StatsWeb.LayoutView do
-  use StatsWeb, :view
-
-  # Phoenix LiveDashboard is available only in development by default,
-  # so we instruct Elixir to not warn if the dashboard route is missing.
-  @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
-end
-
 defmodule ObanUi.Live.Jobs.Index do
   @moduledoc """
   Jobs LiveView
